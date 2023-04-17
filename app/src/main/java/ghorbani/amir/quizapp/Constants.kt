a76983e738ca869f96670bd4f2c2172b8710089a
Amirabbas-Ghorbani <amirabbas.ghorbani67@gmail.com>
2023-04-17 18:03:29 +0200

    first commit

---
+++ app/src/main/java/ghorbani/amir/quizapp/Constants.kt
@@ -0,0 +1,92 @@
+package ghorbani.amir.quizapp
+
+object Constants {
+
+    fun getQuestions() : ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        // 1
+        val que1 = Question(
+            1, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Austria",
+            "Australia", "Armenia",
+            1)
+        questionsList.add(que1)
+
+        // 2
+        val que2 = Question(
+            2, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "England", "Austria",
+            "Australia", "Canada",
+            3)
+        questionsList.add(que2)
+
+        // 3
+        val que3 = Question(
+            3, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bulgaria", "Belgium",
+            "Hungary", "Germany",
+            2)
+        questionsList.add(que3)
+
+        // 4
+        val que4 = Question(
+            4, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Brazil", "Ireland",
+            "Jordan", "Mexico",
+            1)
+        questionsList.add(que4)
+
+        // 5
+        val que5 = Question(
+            5, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Belarus", "Sweden",
+            "Finland", "Denmark",
+            4)
+        questionsList.add(que5)
+
+        // 6
+        val que6 = Question(
+            6, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Australia", "New Zealand",
+            "Fiji", "none of these",
+            3)
+        questionsList.add(que6)
+
+        // 7
+        val que7 = Question(
+            7, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Belgium", "Germany",
+            "Italy", "Netherlands",
+            2)
+        questionsList.add(que7)
+
+        // 8
+        val que8 = Question(
+            8, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "India", "Iran",
+            "Iraq", "Indonesia",
+            1)
+        questionsList.add(que8)
+
+        // 9
+        val que9 = Question(
+            9, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Iraq", "Kuwait",
+            "United Arab Emirates", "none of these",
+            2)
+        questionsList.add(que9)
+
+        return questionsList
+
+    }
+}
---
+++ app/src/main/java/ghorbani/amir/quizapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package ghorbani.amir.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btn_Start)
+        val etEditTextBox : EditText = findViewById(R.id.et_EditTextBox)
+
+        btnStart.setOnClickListener {
+
+            if (etEditTextBox.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name.", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
